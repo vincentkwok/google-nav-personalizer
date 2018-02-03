444969905533fcc3592e2df038bc0ab3653fdb7d
Vincent Kwok <vincentkwok@users.noreply.github.com>
2018-02-03 23:21:36 +0800

    Save option after sort

--- scripts/options.js
+++ scripts/options.js
@@ -1,8 +1,17 @@
 'use strict';
 
-(function($) {
+(function($, chrome) {
   $(document).ready(function() {
-    $('#sortable').sortable({revert: true});
+    $('#sortable').sortable({
+        revert: true,
+        update: function(event, ui) {
+            var resultArray = $(this).sortable('toArray');
+            chrome.storage.sync.set({ 'google-nav-bar-order': resultArray }, function() {
+                // TODO: message about saved
+                console.log('saved');
+            });
+        }
+    });
   });
 
-})(jQuery);
+})(jQuery, chrome);
